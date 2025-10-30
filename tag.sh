002932bda840c56d4705255b9649ecae925bba1a
Dirk Elmendorf <dirk@r26d.com>
2025-10-30 13:53:10 -0500

    Fixed docs

--- tag.sh
+++ tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i  "s/sops-action:v.*/sops-action:v${2}\"/" action.yml
-sed -i  "s/sops-action:v.*/sops-action:v${2}\"/" README.md
+sed -i  "s/sops-action@v.*/sops-action@v${2}\"/" README.md
 git commit -a -m "${1}"
 git tag -a -m "${1}" "v${2}"
 git push --follow-tags
